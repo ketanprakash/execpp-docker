a3f3ce78b0275c58a9b968ff9a5f6939cf96efbd
Ketan Prakash <ketanprakash001@gmail.com>
2023-02-12 23:18:23 +0530

    third commit

--- index.mjs
+++ index.mjs
@@ -10,59 +10,61 @@ const execAsync = util.promisify(exec);
 export const handler = async (event) => {
   // TODO implement
 
-  // const response = {
-  //     statusCode: 200,
-  //     body: JSON.stringify(event)
-  // }
+  const {code, input = ""} = JSON.parse(event["body"]);
 
-  // return response;
-
-  try {
-    const fileName = await generateFileName();
-    const { code, input = "" } = JSON.parse(event["body"]);
-
-    const codePath = path.join(tmpDir, fileName + ".cpp");
-    const inputPath = path.join(tmpDir, fileName + ".txt");
-    const exePath = path.join(tmpDir, fileName + ".exe");
-
-    await fs.writeFile(codePath, code, function (err) {
-      if (err) throw err;
-    });
-    await fs.writeFile(inputPath, input, function (err) {
-      if (err) throw err;
-    });
-
-    await execAsync(`g++ ${codePath} -o ${exePath}`);
-
-    const { error, stdout, stderr } = await execAsync(
-      `${exePath} < ${inputPath}`
-    ).catch((error) => {
-      throw error;
-    });
-
-    const output = error || stderr || stdout;
-    await fs.unlink(codePath);
-    await fs.unlink(inputPath);
-    const response = {
+  const response = {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      isBase64Encoded: false,
-      body: JSON.stringify({ data: output, status: true }),
-    };
-
-    return response;
-  } catch (err) {
-    const response = {
-      statusCode: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      isBase64Encoded: false,
-      body: JSON.stringify({ data: `${err.message}`, status: false }),
-    };
-
-    return response;
+      code: JSON.stringify(code)
   }
+
+  return response;
+
+  // try {
+  //   const fileName = await generateFileName();
+  //   const { code, input = "" } = JSON.parse(event["body"]);
+
+  //   const codePath = path.join(tmpDir, fileName + ".cpp");
+  //   const inputPath = path.join(tmpDir, fileName + ".txt");
+  //   const exePath = path.join(tmpDir, fileName + ".exe");
+
+  //   await fs.writeFile(codePath, code, function (err) {
+  //     if (err) throw err;
+  //   });
+  //   await fs.writeFile(inputPath, input, function (err) {
+  //     if (err) throw err;
+  //   });
+
+  //   await execAsync(`g++ ${codePath} -o ${exePath}`);
+
+  //   const { error, stdout, stderr } = await execAsync(
+  //     `${exePath} < ${inputPath}`
+  //   ).catch((error) => {
+  //     throw error;
+  //   });
+
+  //   const output = error || stderr || stdout;
+  //   await fs.unlink(codePath);
+  //   await fs.unlink(inputPath);
+  //   const response = {
+  //     statusCode: 200,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     isBase64Encoded: false,
+  //     body: JSON.stringify({ data: output, status: true }),
+  //   };
+
+  //   return response;
+  // } catch (err) {
+  //   const response = {
+  //     statusCode: 500,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     isBase64Encoded: false,
+  //     body: JSON.stringify({ data: `${err.message}`, status: false }),
+  //   };
+
+  //   return response;
+  // }
 };
