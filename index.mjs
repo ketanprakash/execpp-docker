b218db0b290444d74e0aa459afcbb0970bf50ee6
Ketan Prakash <ketanprakash001@gmail.com>
2023-02-13 02:33:09 +0530

    ok

--- index.mjs
+++ index.mjs
@@ -21,35 +21,35 @@ export const handler = async (event) => {
     const fileName = await generateFileName();
     const { code, input = "" } = JSON.parse(event["body"]);
 
-    const codePath = path.join(tmpDir, fileName + ".cpp");
-    const inputPath = path.join(tmpDir, fileName + ".txt");
-    const exePath = path.join(tmpDir, fileName + ".exe");
+    // const codePath = path.join(tmpDir, fileName + ".cpp");
+    // const inputPath = path.join(tmpDir, fileName + ".txt");
+    // const exePath = path.join(tmpDir, fileName + ".exe");
 
-    await fs.writeFile(codePath, code, function (err) {
-      if (err) throw err;
-    });
-    await fs.writeFile(inputPath, input, function (err) {
-      if (err) throw err;
-    });
+    // await fs.writeFile(codePath, code, function (err) {
+    //   if (err) throw err;
+    // });
+    // await fs.writeFile(inputPath, input, function (err) {
+    //   if (err) throw err;
+    // });
 
-    await execAsync(`g++ ${codePath} -o ${exePath}`);
+    // await execAsync(`g++ ${codePath} -o ${exePath}`);
 
-    const { error, stdout, stderr } = await execAsync(
-      `${exePath} < ${inputPath}`
-    ).catch((error) => {
-      throw error;
-    });
+    // const { error, stdout, stderr } = await execAsync(
+    //   `${exePath} < ${inputPath}`
+    // ).catch((error) => {
+    //   throw error;
+    // });
 
-    const output = error || stderr || stdout;
-    await fs.unlink(codePath);
-    await fs.unlink(inputPath);
+    // const output = error || stderr || stdout;
+    // await fs.unlink(codePath);
+    // await fs.unlink(inputPath);
     const response = {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
       },
       isBase64Encoded: false,
-      body: JSON.stringify({ data: output, status: true }),
+      body: JSON.stringify({ data: code, status: true }),
     };
 
     return response;
